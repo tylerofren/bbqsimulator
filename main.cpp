e97fbd40a3b867349437cf784898ca0234b72786
Tyler <tylerofreneo@gmail.com>
2023-05-10 12:37:56 -0700

    Initial

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    sf::Window window(sf::VideoMode(1000, 1000), "Game");
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+    }
+
+
+
+
+
+
+    return 0;
+}
