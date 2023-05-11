1c1f371a8b26ae688d678637a7e4871466321086
Philip <philip.neumann05@gmail.com>
2023-05-11 13:10:33 -0700

    Closses when window is opened

--- main.cpp
+++ main.cpp
@@ -2,13 +2,44 @@
 #include <SFML/Audio.hpp>
 #include <iostream>
 
+
+
 using namespace std;
+
+
+const int rows = 5;
+const int cols = 5;
 
 int main()
 {
     
-    sf::Window window(sf::VideoMode(1000, 1000), "Game");
+    sf::RenderWindow window(sf::VideoMode(500, 500), "Stephens Sausage Roll");
 
+    /*
+    string mapLayout[rows][cols];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+          cout << "Enter a string for row " << i << ", column "  << j << ": ";
+          cin >> mapLayout[i][j];
+        }
+    }
+    */
+
+
+    sf::Texture texture;
+    if(!texture.loadFromFile("WaterPlaceholder.png"))
+    {
+        cout << "failed to load image" << endl;
+        exit(1);
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+
+    
     while(window.isOpen())
     {
         sf::Event event;
@@ -16,8 +47,11 @@ int main()
         {
             if (event.type == sf::Event::Closed) window.close();
         }
+        window.clear(sf::Color(255, 255, 255));
+        window.draw(sprite);
+        window.display();
     }
-
+    
 
 
 
