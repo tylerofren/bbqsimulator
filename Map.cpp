e8735a709645211b691c45bd2f1e500573aeced5
Tyler <tylerofreneo@gmail.com>
2023-05-18 15:55:04 -0700

    Changed map, test level is same as level one in ssr

--- Map.cpp
+++ Map.cpp
@@ -14,26 +14,41 @@ Map::Map(std::vector<std::vector<int>> array)
 // Creates a vector of GameTiles using the array
 void Map::initializeTiles(std::vector<std::vector<int>> array) 
 {
-    errorFile.open("errors.txt");
+    /*
+        0 - WATER
+        1 - GROUND
+        2 - GRILL
+        3 - SAUSAGE
+        4 - PLAYER
+        5 - SPAWN
+    */
    for(int i = 0; i < 10; i++)
    {
         for(int j = 0; j < 10; j++)
         {
             
-            if(array[i][j] == 0)
-            {
-                tiles[i][j] = new GameTile("assets/WaterPlaceholder.png", 50.f * j, 50.f * i);
-            }
-            
-            else // (array[i][j] == 1)
+            if(array[i][j] == 1)
             {
                 tiles[i][j] = new GameTile("assets/GroundPlaceholder.png", 50.f * j, 50.f * i);
             }
+            else if(array[i][j] == 2)
+            {
+                tiles[i][j] = new GameTile("assets/grill.png", 50.f * j, 50.f * i);
+            }
+            else if(array[i][j] == 5)
+            {
+                tiles[i][j] = new GameTile("assets/SpawnPlaceholder.png", 50.f * j, 50.f * i);
+            }
+            
+            
+            else 
+            {
+                tiles[i][j] = new GameTile("assets/water.png", 50.f * j, 50.f * i);
+            }
 
-            errorFile << array[i][j];
             
         }
-        errorFile << "\n";
+
    }
-   errorFile.close();
+
 }
